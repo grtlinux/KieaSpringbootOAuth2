1c2be6a90608b09a00b711b3724fbf59d503f8c9
grtlinux <grtlinux@gmail.com>
2020-06-01 21:17:40 +0900

    create first

---
+++ ver-0.1/KieaSpringbootH2/src/main/java/org/tain/KieaSpringbootH2Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaSpringbootH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaSpringbootH2Application.class, args);
+	}
+
+}
