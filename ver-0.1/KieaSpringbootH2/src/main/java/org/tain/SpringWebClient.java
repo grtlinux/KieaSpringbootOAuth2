3a0248099644cc746e2bc3b65bd250161c0bafca
grtlinux <grtlinux@gmail.com>
2020-06-03 13:46:13 +0900

    add WebClient-2020-06-03

--- ver-0.1/KieaSpringbootH2/src/main/java/org/tain/SpringWebClient.java
+++ ver-0.1/KieaSpringbootH2/src/main/java/org/tain/SpringWebClient.java
@@ -1,17 +1,27 @@
 package org.tain;
 
-import org.springframework.http.HttpHeaders;
+import java.util.List;
+
 import org.springframework.web.reactive.function.client.WebClient;
+import org.tain.domain.Stmt;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class SpringWebClient {
 
 	private static boolean flag = true;
 	
 	public static void main(String[] args) throws Exception {
-		if (flag) test01();
+		if (!flag) test01();
+		if (flag) test02();
 	}
 	
+	//////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////
 	private static void test01() throws Exception {
+		/*
 		WebClient webClient = null;
 		webClient = WebClient.create();
 		webClient = WebClient.create("https://api.github.com");
@@ -23,6 +33,7 @@ public class SpringWebClient {
 		
 		String username = "";
 		String token = "";
+		*/
 		
 		// https://github.com/grtlinux/spring-webclient-webtestclient-demo
 		/*
@@ -33,4 +44,66 @@ public class SpringWebClient {
 				.bodyToFlux(GithubRepo.Class);
 		*/
 	}
+	
+	//////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////
+	private WebClient webClient;
+	
+	private SpringWebClient() {
+		//this.webClient = WebClient.create();
+		this.webClient = WebClient.create("http://localhost:8080/api");
+		//this.webClient = WebClient.builder()
+		//		.baseUrl("http://localhost:8080/api")
+		//		.defaultHeader(HttpHeaders.CONTENT_TYPE, "application/vnd.github.v3+json")
+		//		.defaultHeader(HttpHeaders.USER_AGENT, "Spring 5 WebClient")
+		//		.build();
+	}
+	
+	private static void test02() {
+		SpringWebClient springWebClient = new SpringWebClient();
+		
+		springWebClient.findAll();
+		springWebClient.findById(4);
+		//springWebClient.create(Stmt.join(1, 1101, "", ""));
+		//springWebClient.update(Stmt.join(1, 1101, "", ""));
+		//springWebClient.delete(4);
+	}
+
+	private void findAll() {
+		Flux<Stmt> fluxStmt = this.webClient.get()
+				.uri("/stmts")
+				.retrieve()
+				.bodyToFlux(Stmt.class);
+		List<Stmt> lstStmt = fluxStmt.collectList().block();
+		//lstStmt.forEach(System.out::println);
+		for (Stmt stmt : lstStmt) {
+			System.out.println(">>>>> " + stmt);
+		}
+	}
+
+	private void findById(Integer id) {
+		Mono<Stmt> monoStmt = this.webClient.get()
+				.uri("/stmts/" + id)
+				.retrieve()
+				.bodyToMono(Stmt.class);
+		System.out.println(">>>>> " + monoStmt);
+	}
+
+	private void create(Stmt join) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void update(Stmt join) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void delete(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
